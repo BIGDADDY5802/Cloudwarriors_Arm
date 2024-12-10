e5ae0c7bff3dffd371afaa05997a9f3b78d19659
jorge341 <137249059+jorge341@users.noreply.github.com>
2024-12-09 22:14:00 -0500

    Update 7-SG01-All.tf

--- 7-SG01-All.tf
+++ 7-SG01-All.tf
@@ -38,14 +38,11 @@ resource "aws_security_group" "app1-sg01-servers" {
   tags = {
     Name    = "app1-sg01-servers"
     Service = "application1"
-    Owner   = "Luke"
-    Planet  = "Musafar"
+    Owner   = "Papi Chulo"
+    Planet  = "Uranus"
   }
 
 }
-
-
-
 
 
 resource "aws_security_group" "app1-sg02-LB01" {
@@ -79,8 +76,512 @@ resource "aws_security_group" "app1-sg02-LB01" {
   tags = {
     Name    = "app1-sg02-LB01"
     Service = "application1"
-    Owner   = "Luke"
-    Planet  = "Musafar"
+    Owner   = "Papi Chulo"
+    Planet  = "Uranus"
+  }
+
+}
+
+resource "aws_security_group" "app2-sg01-servers" {
+  name        = "app2-sg01-servers"
+  description = "app2-sg01-servers"
+  vpc_id      = aws_vpc.app2.id
+
+  ingress {
+    description = "MyHomePage"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "MyEvilBox"
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "app2-sg01-servers"
+    Service = "application2"
+    Owner   = "Papi Chulo"
+    Planet  = "Uranus"
+  }
+
+}
+
+
+resource "aws_security_group" "app2-sg02-LB01" {
+  name        = "app2-sg02-LB01"
+  description = "app2-sg02-LB01"
+  vpc_id      = aws_vpc.app2.id
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "app2-sg02-LB01"
+    Service = "application2"
+    Owner   = "Papi Chulo"
+    Planet  = "Uranus"
+  }
+
+}
+
+resource "aws_security_group" "app3-sg01-servers" {
+  name        = "app3-sg01-servers"
+  description = "app3-sg01-servers"
+  vpc_id      = aws_vpc.app3.id
+
+  ingress {
+    description = "MyHomePage"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "MyEvilBox"
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "app3-sg01-servers"
+    Service = "application3"
+    Owner   = "Papi Chulo"
+    Planet  = "Uranus"
+  }
+
+}
+
+
+resource "aws_security_group" "app3-sg02-LB01" {
+  name        = "app3-sg02-LB01"
+  description = "app3-sg02-LB01"
+  vpc_id      = aws_vpc.app3.id
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "app3-sg02-LB01"
+    Service = "application3"
+    Owner   = "Papi Chulo"
+    Planet  = "Uranus"
+  }
+
+}
+
+resource "aws_security_group" "app4-sg01-servers" {
+  name        = "app4-sg01-servers"
+  description = "app4-sg01-servers"
+  vpc_id      = aws_vpc.app4.id
+
+  ingress {
+    description = "MyHomePage"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "MyEvilBox"
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "app4-sg01-servers"
+    Service = "application4"
+    Owner   = "Papi Chulo"
+    Planet  = "Uranus"
+  }
+
+}
+
+
+resource "aws_security_group" "app4-sg02-LB01" {
+  name        = "app4-sg02-LB01"
+  description = "app4-sg02-LB01"
+  vpc_id      = aws_vpc.app4.id
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "app4-sg02-LB01"
+    Service = "application4"
+    Owner   = "Papi Chulo"
+    Planet  = "Uranus"
+  }
+
+}
+
+resource "aws_security_group" "app5-sg01-servers" {
+  name        = "app5-sg01-servers"
+  description = "app5-sg01-servers"
+  vpc_id      = aws_vpc.app5.id
+
+  ingress {
+    description = "MyHomePage"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "MyEvilBox"
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "app5-sg01-servers"
+    Service = "application5"
+    Owner   = "Papi Chulo"
+    Planet  = "Uranus"
+  }
+
+}
+
+
+resource "aws_security_group" "app5-sg02-LB01" {
+  name        = "app5-sg02-LB01"
+  description = "app5-sg02-LB01"
+  vpc_id      = aws_vpc.app5.id
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "app5-sg02-LB01"
+    Service = "application5"
+    Owner   = "Papi Chulo"
+    Planet  = "Uranus"
+  }
+
+}
+
+resource "aws_security_group" "app6-sg01-servers" {
+  name        = "app6-sg01-servers"
+  description = "app6-sg01-servers"
+  vpc_id      = aws_vpc.app6.id
+
+  ingress {
+    description = "MyHomePage"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "MyEvilBox"
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "app6-sg01-servers"
+    Service = "application6"
+    Owner   = "Papi Chulo"
+    Planet  = "Uranus"
+  }
+
+}
+
+
+resource "aws_security_group" "app6-sg02-LB01" {
+  name        = "app6-sg02-LB01"
+  description = "app6-sg02-LB01"
+  vpc_id      = aws_vpc.app6.id
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "app6-sg02-LB01"
+    Service = "application6"
+    Owner   = "Papi Chulo"
+    Planet  = "Uranus"
+  }
+
+}
+
+resource "aws_security_group" "app7-sg01-servers" {
+  name        = "app7-sg01-servers"
+  description = "app7-sg01-servers"
+  vpc_id      = aws_vpc.app7.id
+
+  ingress {
+    description = "MyHomePage"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "MyEvilBox"
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "app7-sg01-servers"
+    Service = "application7"
+    Owner   = "Papi Chulo"
+    Planet  = "Uranus"
+  }
+
+}
+
+
+resource "aws_security_group" "app7-sg02-LB01" {
+  name        = "app7-sg02-LB01"
+  description = "app7-sg02-LB01"
+  vpc_id      = aws_vpc.app7.id
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "app7-sg02-LB01"
+    Service = "application7"
+    Owner   = "Papi Chulo"
+    Planet  = "Uranus"
   }
 
 }
