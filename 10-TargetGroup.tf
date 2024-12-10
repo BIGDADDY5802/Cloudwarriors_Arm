a0076b5d782c3ab03074869840a0b257c1259422
jorge341 <137249059+jorge341@users.noreply.github.com>
2024-12-09 22:18:14 -0500

    Update 10-TargetGroup.tf

--- 10-TargetGroup.tf
+++ 10-TargetGroup.tf
@@ -1,4 +1,4 @@
-resource "aws_lb_target_group" "app1_tg" {
+/*resource "aws_lb_target_group" "app1_tg" {
   name     = "app1-target-group"
   port     = 80
   protocol = "HTTP"
@@ -21,5 +21,57 @@ resource "aws_lb_target_group" "app1_tg" {
     Service = "App1"
     Owner   = "User"
     Project = "Web Service"
+  }
+}*/
+
+resource "aws_lb_target_group" "app1_tg" {
+  name     = "app1-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.app1.id
+  target_type = "instance"
+
+  health_check {
+    enabled             = true
+    interval            = 30
+    path                = "/"
+    protocol            = "HTTP"
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+    timeout             = 5
+    matcher             = "200"
+  }
+
+  tags = {
+    Name    = "App1TargetGroup"
+    Service = "App1"
+    Owner   = "Papi Chulo"
+    Project = "App1"
+  }
+}
+
+resource "aws_lb_target_group" "app1_tg_443" {
+  name     = "app1tg443"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.app1.id
+  target_type = "instance"
+
+  health_check {
+    enabled             = true
+    interval            = 30
+    path                = "/"
+    protocol            = "HTTP"
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+    timeout             = 5
+    matcher             = "200"
+  }
+
+  tags = {
+    Name    = "app1_tg_443"
+    Service = "App1"
+    Owner   = "Papi Chulo"
+    Project = "App1"
   }
 }
