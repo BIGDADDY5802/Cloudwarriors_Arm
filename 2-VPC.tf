0d443a598655a4bcfd42ee8e362ca9ef2aaef0b1
jorge341 <137249059+jorge341@users.noreply.github.com>
2024-12-09 22:07:38 -0500

    Update 2-VPC.tf

--- 2-VPC.tf
+++ 2-VPC.tf
@@ -21,57 +21,58 @@ resource "aws_vpc" "app2" {
   }
 }
 
-# resource "aws_vpc" "app3" {
-#   cidr_block = "10.192.0.0/16"
+resource "aws_vpc" "app3" {
+  cidr_block = "10.192.0.0/16"
 
-#   tags = {
-#     Name = "app3"
-#     Service = "application3"
-#     Owner = "Papi-Chulo"
-#     Planet = "Uranus"
-#   }
-# }
+  tags = {
+    Name = "app3"
+    Service = "application3"
+    Owner = "Papi-Chulo"
+    Planet = "Uranus"
+  }
+}
 
-# resource "aws_vpc" "app4" {
-#   cidr_block = "10.193.0.0/16"
+resource "aws_vpc" "app4" {
+  cidr_block = "10.193.0.0/16"
 
-#   tags = {
-#     Name = "app4"
-#     Service = "application4"
-#     Owner = "Papi-Chulo"
-#     Planet = "Uranus"
-#   }
-# }
+  tags = {
+    Name = "app4"
+    Service = "application4"
+    Owner = "Papi-Chulo"
+    Planet = "Uranus"
+  }
+}
 
-# resource "aws_vpc" "app5" {
-#   cidr_block = "10.194.0.0/16"
+resource "aws_vpc" "app5" {
+  cidr_block = "10.194.0.0/16"
 
-#   tags = {
-#     Name = "app5"
-#     Service = "application5"
-#     Owner = "Papi-Chulo"
-#     Planet = "Uranus"
-#   }
-# }
+  tags = {
+    Name = "app5"
+    Service = "application5"
+    Owner = "Papi-Chulo"
+    Planet = "Uranus"
+  }
+}
 
-# resource "aws_vpc" "app6" {
-#   cidr_block = "10.195.0.0/16"
+resource "aws_vpc" "app6" {
+  cidr_block = "10.195.0.0/16"
 
-#   tags = {
-#     Name = "app6"
-#     Service = "application6"
-#     Owner = "Papi-Chulo"
-#     Planet = "Uranus"
-#   }
-# }
+  tags = {
+    Name = "app6"
+    Service = "application6"
+    Owner = "Papi-Chulo"
+    Planet = "Uranus"
+  }
+}
 
-# resource "aws_vpc" "app7" {
-#   cidr_block = "10.196.0.0/16"
+resource "aws_vpc" "app7" {
+  cidr_block = "10.196.0.0/16"
 
-#   tags = {
-#     Name = "app7"
-#     Service = "application7"
-#     Owner = "Papi-Chulo"
-#     Planet = "Uranus"
-#   }
+  tags = {
+    Name = "app7"
+    Service = "application7"
+    Owner = "Papi-Chulo"
+    Planet = "Uranus"
+  }
+}
 # }
