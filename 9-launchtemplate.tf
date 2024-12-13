53474eebab16ed86112951745ed4b424c9490843
jorge341 <137249059+jorge341@users.noreply.github.com>
2024-12-13 11:57:26 -0500

    Update 9-launchtemplate.tf

--- 9-launchtemplate.tf
+++ 9-launchtemplate.tf
@@ -1,73 +1,3 @@
-/*resource "aws_launch_template" "app1_LT" {
-  name_prefix   = "app1_LT"
-  image_id      = "ami-063d43db0594b521b"  
-  instance_type = "t2.micro"
-
-  key_name = "MyLinuxBox"
-
-  vpc_security_group_ids = [aws_security_group.app1-sg01-servers.id]
-
-  user_data = base64encode(<<-EOF
-    #!/bin/bash
-    yum update -y
-    yum install -y httpd
-    systemctl start httpd
-    systemctl enable httpd
-
-    # Get the IMDSv2 token
-    TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
-
-    # Background the curl requests
-    curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/local-ipv4 &> /tmp/local_ipv4 &
-    curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/placement/availability-zone &> /tmp/az &
-    curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/ &> /tmp/macid &
-    wait
-
-    macid=$(cat /tmp/macid)
-    local_ipv4=$(cat /tmp/local_ipv4)
-    az=$(cat /tmp/az)
-    vpc=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/$macid/vpc-id)
-
-    # Create HTML file
-    cat <<-HTML > /var/www/html/index.html
-    <!doctype html>
-    <html lang="en" class="h-100">
-    <head>
-    <title>Details for EC2 instance</title>
-    </head>
-    <body>
-    <div>
-    <h1>Papi Chulo</h1>
-    <h1>Chains Broken in Virginia</h1>
-    <p><b>Instance Name:</b> $(hostname -f) </p>
-    <p><b>Instance Private Ip Address: </b> $local_ipv4</p>
-    <p><b>Availability Zone: </b> $az</p>
-    <p><b>Virtual Private Cloud (VPC):</b> $vpc</p>
-    </div>
-    </body>
-    </html>
-    HTML
-
-    # Clean up the temp files
-    rm -f /tmp/local_ipv4 /tmp/az /tmp/macid
-  EOF
-  )
-
-  tag_specifications {
-    resource_type = "instance"
-    tags = {
-      Name    = "app1_LT"
-      Service = "application1"
-      Owner   = "Chewbacca"
-      Planet  = "Mustafar"
-    }
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}*/
-
 resource "aws_launch_template" "app1_LT" {
   name_prefix   = "app1_LT"
   image_id      = "ami-023ff3d4ab11b2525"  
@@ -219,7 +149,7 @@ resource "aws_launch_template" "app2_LT" {
 
   key_name = "MyLinuxBox"
 
-  vpc_security_group_ids = [aws_security_group.app1-sg01-servers.id]
+  vpc_security_group_ids = [aws_security_group.app2-sg01-servers.id]
 
   user_data = base64encode(<<-EOF
     #!/bin/bash
@@ -292,7 +222,7 @@ resource "aws_launch_template" "app2_LT_443" {
 
   key_name = "MyLinuxBox"
 
-  vpc_security_group_ids = [aws_security_group.app1-sg03-secure-servers.id]
+  vpc_security_group_ids = [aws_security_group.app2-sg03-secure-servers.id]
 
   user_data = base64encode(<<-EOF
     #!/bin/bash
@@ -363,7 +293,7 @@ resource "aws_launch_template" "app3_LT" {
 
   key_name = "MyLinuxBox"
 
-  vpc_security_group_ids = [aws_security_group.app1-sg01-servers.id]
+  vpc_security_group_ids = [aws_security_group.app3-sg01-servers.id]
 
   user_data = base64encode(<<-EOF
     #!/bin/bash
@@ -436,7 +366,7 @@ resource "aws_launch_template" "app3_LT_443" {
 
   key_name = "MyLinuxBox"
 
-  vpc_security_group_ids = [aws_security_group.app1-sg03-secure-servers.id]
+  vpc_security_group_ids = [aws_security_group.app3-sg03-secure-servers.id]
 
   user_data = base64encode(<<-EOF
     #!/bin/bash
@@ -507,7 +437,7 @@ resource "aws_launch_template" "app4_LT" {
 
   key_name = "MyLinuxBox"
 
-  vpc_security_group_ids = [aws_security_group.app1-sg01-servers.id]
+  vpc_security_group_ids = [aws_security_group.app4-sg01-servers.id]
 
   user_data = base64encode(<<-EOF
     #!/bin/bash
@@ -580,7 +510,7 @@ resource "aws_launch_template" "app4_LT_443" {
 
   key_name = "MyLinuxBox"
 
-  vpc_security_group_ids = [aws_security_group.app1-sg03-secure-servers.id]
+  vpc_security_group_ids = [aws_security_group.app4-sg03-secure-servers.id]
 
   user_data = base64encode(<<-EOF
     #!/bin/bash
@@ -651,7 +581,7 @@ resource "aws_launch_template" "app5_LT" {
 
   key_name = "MyLinuxBox"
 
-  vpc_security_group_ids = [aws_security_group.app1-sg01-servers.id]
+  vpc_security_group_ids = [aws_security_group.app5-sg01-servers.id]
 
   user_data = base64encode(<<-EOF
     #!/bin/bash
@@ -724,7 +654,7 @@ resource "aws_launch_template" "app5_LT_443" {
 
   key_name = "MyLinuxBox"
 
-  vpc_security_group_ids = [aws_security_group.app1-sg03-secure-servers.id]
+  vpc_security_group_ids = [aws_security_group.app5-sg03-secure-servers.id]
 
   user_data = base64encode(<<-EOF
     #!/bin/bash
@@ -795,7 +725,7 @@ resource "aws_launch_template" "app6_LT" {
 
   key_name = "MyLinuxBox"
 
-  vpc_security_group_ids = [aws_security_group.app1-sg01-servers.id]
+  vpc_security_group_ids = [aws_security_group.app6-sg01-servers.id]
 
   user_data = base64encode(<<-EOF
     #!/bin/bash
@@ -867,7 +797,7 @@ resource "aws_launch_template" "app6_LT_443" {
 
   key_name = "MyLinuxBox"
 
-  vpc_security_group_ids = [aws_security_group.app1-sg03-secure-servers.id]
+  vpc_security_group_ids = [aws_security_group.app6-sg03-secure-servers.id]
 
   user_data = base64encode(<<-EOF
     #!/bin/bash
@@ -938,7 +868,7 @@ resource "aws_launch_template" "app7_LT" {
 
   key_name = "MyLinuxBox"
 
-  vpc_security_group_ids = [aws_security_group.app1-sg01-servers.id]
+  vpc_security_group_ids = [aws_security_group.app7-sg01-servers.id]
 
   user_data = base64encode(<<-EOF
     #!/bin/bash
@@ -1011,7 +941,7 @@ resource "aws_launch_template" "app7_LT_443" {
 
   key_name = "MyLinuxBox"
 
-  vpc_security_group_ids = [aws_security_group.app1-sg03-secure-servers.id]
+  vpc_security_group_ids = [aws_security_group.app7-sg03-secure-servers.id]
 
   user_data = base64encode(<<-EOF
     #!/bin/bash
